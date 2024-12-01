7ae56b1e62067e65ed5a9d18ca705da26d8b142a
Joeperson54321 <zrichmnd@gmail.com>
2024-12-01 14:12:13 -0600

    Add files via upload

---
+++ blockblast.py
@@ -0,0 +1,11 @@
+import pygame;
+pygame.init();
+pygame.display.set_caption("Block Blast");
+screen = pygame.display.set_mode((400,300));
+pygame.draw.rect(screen,(25,50,50),pygame.Rect(100,100,50,50))
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
+pygame.quit()
