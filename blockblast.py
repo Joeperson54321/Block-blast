693ca403839af9f946b7b0af1de3f2ac422c6574
Joeperson54321 <zrichmnd@gmail.com>
2024-12-01 14:51:39 -0600

    Update blockblast.py

--- blockblast.py
+++ blockblast.py
@@ -2,7 +2,9 @@ import pygame
 pygame.init()
 pygame.display.set_caption("Block Blast")
 screen = pygame.display.set_mode((400,300))
-pygame.draw.rect(screen,(25,50,50),pygame.Rect(100,100,50,50))
+def drawSquare(x,y,r,g,b,len):
+    pygame.draw.rect(screen,(r,g,b),pygame.Rect(x,y,len,len))
+drawSquare(100,100,200,100,100,50)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
