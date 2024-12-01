6838bbc0c90bd35b041a90a2bca8147c3d41d6a4
Joeperson54321 <zrichmnd@gmail.com>
2024-12-01 14:33:23 -0600

    Update blockblast.py

--- blockblast.py
+++ blockblast.py
@@ -1,7 +1,7 @@
-import pygame;
-pygame.init();
-pygame.display.set_caption("Block Blast");
-screen = pygame.display.set_mode((400,300));
+import pygame
+pygame.init()
+pygame.display.set_caption("Block Blast")
+screen = pygame.display.set_mode((400,300))
 pygame.draw.rect(screen,(25,50,50),pygame.Rect(100,100,50,50))
 while True:
     for event in pygame.event.get():
