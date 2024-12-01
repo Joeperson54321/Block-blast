4adb5d176b943a27a08262a4768c5051ea723e03
Joeperson54321 <zrichmnd@gmail.com>
2024-12-01 15:58:27 -0600

    Update blockblast.py

--- blockblast.py
+++ blockblast.py
@@ -1,13 +1,33 @@
-import pygame
+import pygame,sys
+spaces = []
+colorsr = []
+colorsg = []
+colorsb = []
+for i in range(64):
+    spaces.append(0)
+    colorsr.append(10)
+    colorsg.append(10)
+    colorsb.append(120)
 pygame.init()
 pygame.display.set_caption("Block Blast")
-screen = pygame.display.set_mode((400,300))
+screen = pygame.display.set_mode((800,500))
 def drawSquare(x,y,r,g,b,len):
     pygame.draw.rect(screen,(r,g,b),pygame.Rect(x,y,len,len))
-drawSquare(100,100,200,100,100,50)
+def drawGridSquares():
+    for i in range(8):
+        for j in range(8):
+            index =i*8+j
+            drawSquare(50+j*25,50+i*25,colorsr[index],colorsg[index],colorsb[index],20)
+def drawGridLines():
+    print("left")
+drawSquare(100,100,255,0,0,50)
+#pygame.draw.rect(screen,(100,100,50),pygame.Rect(50,50,50,50)) 
 while True:
+    #pygame.quit()
+    screen.fill((0,0,0))
+    drawGridSquares()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
     pygame.display.update()
-pygame.quit()
